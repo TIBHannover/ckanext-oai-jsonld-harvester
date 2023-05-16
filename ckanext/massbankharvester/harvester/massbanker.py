ee29dfaab06759565fb80b516764ed2ef2f921f6
Bhavin Katabathuni <bhavin2897@gmail.com>
2023-05-16 16:07:54 +0200

    test from one guid

--- ckanext/massbankharvester/harvester/massbanker.py
+++ ckanext/massbankharvester/harvester/massbanker.py
@@ -85,7 +85,7 @@ class MassbankHarvester(HarvesterBase):
                     guid=header.identifier(), job=harvest_job
                 )
                 #TODO: drop if and  break
-                if guid == 'https://massbank.eu/MassBank/RecordDisplay?id=MSBNK-Fac_Eng_Univ_Tokyo-JP002512#VTSZSPVMHBJJIS-UHFFFAOYSA-N':
+                if harvest_obj.guid == 'https://massbank.eu/MassBank/RecordDisplay?id=MSBNK-Fac_Eng_Univ_Tokyo-JP002512#VTSZSPVMHBJJIS-UHFFFAOYSA-N':
                     harvest_obj.save()
                     #harvest_obj_ids.append(harvest_obj.id)
                     log.debug("Harvest obj %s created" % harvest_obj.id)
