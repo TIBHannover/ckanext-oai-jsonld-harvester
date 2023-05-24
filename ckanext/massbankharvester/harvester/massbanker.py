2756b489ea827cd7a47bdcbe5084a3ba4cf1f2e4
Bhavin Katabathuni <bhavin2897@gmail.com>
2023-05-24 09:52:30 +0200

    TEST for nmrXiv id

--- ckanext/massbankharvester/harvester/massbanker.py
+++ ckanext/massbankharvester/harvester/massbanker.py
@@ -86,7 +86,7 @@ class MassbankHarvester(HarvesterBase):
                 )
                 #TODO: drop if and  break for individual identifier.
                 #if harvest_obj.guid == 'https://massbank.eu/MassBank/RecordDisplay?id=MSBNK-Fac_Eng_Univ_Tokyo-JP002512#VTSZSPVMHBJJIS-UHFFFAOYSA-N':
-                if harvest_obj.guid == 'D506b':
+                if harvest_obj.guid == 'D506c':
                     harvest_obj.save()
                     harvest_obj_ids.append(harvest_obj.id)
                     log.debug("Harvest obj %s created" % harvest_obj.id)
